043de5f648d323afcde6747f4454bfeb872933d1
Dirk Elmendorf <dirk@r26d.com>
2020-06-27 07:16:32 -0500

    infra: Updated the Poison Package to 4.0.1

--- mix.exs
+++ mix.exs
@@ -9,13 +9,33 @@ defmodule RasaSdk.Mixfile do
       config_path: "./config/config.exs",
       deps_path: "./deps",
       lockfile: "./mix.lock",
-      elixir: "~> 1.6",
+      elixir: "~> 1.9", # I have only worked on this on 1.9 It may support earlier versions
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
+  defp description do
+   """
+      Rasa is an open source machine learning framework to automate text-and voice-based conversations.
+      https://github.com/RasaHQ/rasa
 
+      This library provides tools to communicate and control a Rasa application from Elixir.
+   """
+
+  end
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Dirk Elmendorf"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/r26D/rasa-sdk-elixir"
+      }
+    ]
+  end
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
@@ -37,8 +57,8 @@ defmodule RasaSdk.Mixfile do
     [
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:tesla, "~> 1.3"},
-      {:poison, "~> 3.1"},
-                {:plug_crypto, "~> 1.1.2"},
+      {:poison, "~> 4.0.1"},
+      {:plug_crypto, "~> 1.1.2"},
       #{:jason, "~> 1.2"},
       {:plug, "~> 1.10"}
     ]
